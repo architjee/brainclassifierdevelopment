4383d45da25c88c7edb077f3e62edf21212bb515
Archit Jain <architjee@gmail.com>
2021-03-26 14:48:22 +0530

    commit

---
+++ index.js
@@ -0,0 +1,142 @@
+import * as tf from '@tensorflow/tfjs';
+
+const MOBILENET_MODEL_PATH =
+    // tslint:disable-next-line:max-line-length
+    'mobilenetmodel/model.json';
+
+const IMAGE_SIZE = 224;
+const TOPK_PREDICTIONS = 1;
+const img_classes = ['no_tumor', 'pituitary_tumor', 'Meningioma_Tumor', 'Glioma_tumor']
+const classes = [];
+let mobilenet;
+const mobilenetDemo = async () => {
+  status('Loading model...');
+  
+
+  mobilenet = await tf.loadLayersModel(MOBILENET_MODEL_PATH);
+  
+  // Warmup the model. This isn't necessary, but makes the first prediction
+  // faster. Call `dispose` to release the WebGL memory allocated for the return
+  // value of `predict`.
+ 
+  mobilenet.predict(tf.zeros([1, IMAGE_SIZE, IMAGE_SIZE, 3])).dispose();
+  
+  status('');
+
+  // Make a prediction through the locally hosted cat.jpg.
+  const catElement = document.getElementById('cat');
+  if (catElement.complete && catElement.naturalHeight !== 0) {
+    predict(catElement);
+    catElement.style.display = '';
+  } else {
+    catElement.onload = () => {
+      predict(catElement);
+      catElement.style.display = '';
+    }
+  }
+
+  document.getElementById('file-container').style.display = '';
+};
+
+/**
+ * Given an image element, makes a prediction through mobilenet returning the
+ * probabilities of the top K classes.
+ */
+async function predict(imgElement) {
+  status('Predicting...');
+
+  // The first start time includes the time it takes to extract the image
+  // from the HTML and preprocess it, in additon to the predict() call.
+  const startTime1 = performance.now();
+  // The second start time excludes the extraction and preprocessing and
+  // includes only the predict() call.
+  let startTime2;
+  const logits = tf.tidy(() => {
+    // tf.browser.fromPixels() returns a Tensor from an image element.
+    const img = tf.browser.fromPixels(imgElement).toFloat();
+
+    
+
+    startTime2 = performance.now();
+    // Make a prediction through mobilenet.
+    const res =  mobilenet.predict(img.reshape([1, IMAGE_SIZE, IMAGE_SIZE, 3])).argMax(1).dataSync()[0];
+    console.log(img_classes[res])
+    // status(img_classes[res])
+    document.getElementById('imp').innerText = img_classes[res];
+    return res;
+  });
+
+  // Convert logits to probabilities and class names.
+
+  const totalTime1 = performance.now() - startTime1;
+  const totalTime2 = performance.now() - startTime2;
+  status(`Done in ${Math.floor(totalTime1)} ms ` +
+      `(not including preprocessing: ${Math.floor(totalTime2)} ms)`);
+
+  // Show the classes in the DOM.
+  showResults(imgElement, classes);
+}
+
+
+
+function showResults(imgElement, classes) {
+  const predictionContainer = document.createElement('div');
+  predictionContainer.className = 'pred-container';
+
+  const imgContainer = document.createElement('div');
+  imgContainer.appendChild(imgElement);
+  predictionContainer.appendChild(imgContainer);
+
+  const probsContainer = document.createElement('div');
+  for (let i = 0; i < classes.length; i++) {
+    const row = document.createElement('div');
+    row.className = 'row';
+
+    const classElement = document.createElement('div');
+    classElement.className = 'cell';
+    classElement.innerText = classes[i].className;
+    row.appendChild(classElement);
+
+    const probsElement = document.createElement('div');
+    probsElement.className = 'cell';
+    probsElement.innerText = classes[i].probability.toFixed(3);
+    row.appendChild(probsElement);
+
+    probsContainer.appendChild(row);
+  }
+  predictionContainer.appendChild(probsContainer);
+
+  predictionsElement.insertBefore(
+      predictionContainer, predictionsElement.firstChild);
+}
+
+const filesElement = document.getElementById('files');
+filesElement.addEventListener('change', evt => {
+  let files = evt.target.files;
+  // Display thumbnails & issue call to predict each image.
+  for (let i = 0, f; f = files[i]; i++) {
+    // Only process image files (skip non image files)
+    if (!f.type.match('image.*')) {
+      continue;
+    }
+    let reader = new FileReader();
+    reader.onload = e => {
+      // Fill the image & call predict.
+      let img = document.createElement('img');
+      img.src = e.target.result;
+      img.width = IMAGE_SIZE;
+      img.height = IMAGE_SIZE;
+      img.onload = () => predict(img);
+    };
+
+    // Read in the image file as a data URL.
+    reader.readAsDataURL(f);
+  }
+});
+
+const demoStatusElement = document.getElementById('status');
+const status = msg => demoStatusElement.innerText = msg;
+
+const predictionsElement = document.getElementById('predictions');
+
+mobilenetDemo();
